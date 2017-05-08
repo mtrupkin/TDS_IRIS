91e4af09d6a443a10561d34a74da970eca956fa6
alexlepinski <alex.lepinski@oregonstate.edu>
2017-05-08 15:18:28 -0700

    updates to travis

--- DeployScripts/code_deploy.sh
+++ DeployScripts/code_deploy.sh
@@ -3,5 +3,5 @@ set -ev
 
 cd docker/
 docker build -f Dockerfile.code -t tds_iriscode .
-docker tag itemviewerservice:latest osucass/tds_iriscode:$BRANCH
+docker tag tds_iriscode:latest osucass/tds_iriscode:$BRANCH
 docker push osucass/tds_iriscode:$BRANCH
