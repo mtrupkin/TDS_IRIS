52c10d9c4c4876a4f61ad59b6467a977777f2d6b
Miller <millcour@oregonstate.edu>
2017-07-19 13:36:25 -0700

    added feature to reload the content if an item is added or changed

--- iris/src/main/java/tds/iris/content/ContentBuilder.java
+++ iris/src/main/java/tds/iris/content/ContentBuilder.java
@@ -1,13 +1,15 @@
 /*******************************************************************************
  * Educational Online Test Delivery System 
  * Copyright (c) 2014 American Institutes for Research
- *       
+ *
  * Distributed under the AIR Open Source License, Version 1.0 
  * See accompanying file AIR-License-1_0.txt or at
  * http://www.smarterapp.org/documents/American_Institutes_for_Research_Open_Source_Software_License.pdf
  ******************************************************************************/
 package tds.iris.content;
 
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
@@ -32,33 +34,39 @@ import tds.itemrenderer.data.IrisITSDocument;
 import tds.itemrenderer.data.ItsItemIdUtil;
 import tds.itemrenderer.data.ITSTypes.ITSEntityType;
 
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
 @Component
-@Scope ("singleton")
-public class ContentBuilder implements IContentBuilder
-{
-  private static final Logger _logger           = LoggerFactory.getLogger (ContentBuilder.class);
-  private String              _contentPath;
-  private ConfigBuilder       _directoryScanner = null;
-  
-  public synchronized void init () throws ContentException {
-    try {
-      // scan the local folder.
-      _contentPath = AppSettingsHelper.get ("iris.ContentPath");
-      _directoryScanner = new ConfigBuilder (_contentPath);
-      _directoryScanner.create ();
-    } catch (Exception exp) {
-      _logger.error ("Error loading IRiS content.", exp);
-      throw new ContentException (exp);
+@Scope("singleton")
+public class ContentBuilder implements IContentBuilder {
+    private static final Logger _logger = LoggerFactory.getLogger(ContentBuilder.class);
+    private String _contentPath;
+    private ConfigBuilder _directoryScanner = null;
+
+    public synchronized void init() throws ContentException {
+        try {
+            // scan the local folder.
+            _contentPath = AppSettingsHelper.get("iris.ContentPath");
+            _directoryScanner = new ConfigBuilder(_contentPath);
+            _directoryScanner.create();
+        } catch (Exception exp) {
+            _logger.error("Error loading IRiS content.", exp);
+            throw new ContentException(exp);
+        }
+
     }
-  }
 
-  @Override
-  public IITSDocument getITSDocument (String id) throws ContentRequestException {
-    return _directoryScanner.getRenderableDocument (id);
-  }
 
-  @Override
-  public IITSDocument getITSDocumentAcc (String id, AccLookup accLookup) {
-    return _directoryScanner.getRenderableDocument(id, accLookup);
-  }
+
+    @Override
+    public IITSDocument getITSDocument(String id) throws ContentRequestException {
+        return _directoryScanner.getRenderableDocument(id);
+    }
+
+    @Override
+    public IITSDocument getITSDocumentAcc(String id, AccLookup accLookup) {
+        return _directoryScanner.getRenderableDocument(id, accLookup);
+    }
 }
